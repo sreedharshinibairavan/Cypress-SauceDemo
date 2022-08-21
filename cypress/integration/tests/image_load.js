57ce5fa51ce48a75d2de47c28fdbaf20b38e1337
sreedharshinibairavan <sreedharshini34@gmail.com>
2022-08-22 00:17:03 +0530

    added ESLint and commands

--- cypress/integration/tests/image_load.js
+++ cypress/integration/tests/image_load.js
@@ -1,13 +1,10 @@
-/// <reference types="Cypress" />
 import '../../support/index.js';
 
 describe('Test if all images in the inventory are loaded fine', () => {
-
-    it('Image load', () => {
-        
-        cy.get('a>img.inventory_item_img')
-            .should('be.visible')
-            .and('have.prop', 'naturalWidth')
-            .should('be.greaterThan', 0)
-    });
+  it('Image load', () => {
+    cy.get('a>img.inventory_item_img')
+        .should('be.visible')
+        .and('have.prop', 'naturalWidth')
+        .should('be.greaterThan', 0);
+  });
 });
--- cypress/integration/tests/test_links.js
+++ cypress/integration/tests/test_links.js
@@ -1,25 +1,22 @@
-/// <reference types="Cypress" />
+// / <reference types="Cypress" />
 import '../../support/index.js';
 
-/*----- SauceDemo Website automation -----*/
+/* ----- SauceDemo Website automation -----*/
 describe('Test all links in the webpage', () => {
-
   it('Verify if any link is broken within the webpage', () => {
-    
-    cy.on('window:confirm', cy.stub().as('confirm'))
+    cy.on('window:confirm', cy.stub().as('confirm'));
     Cypress.on('uncaught:exception', (err, runnable) => {
       // returning false here prevents Cypress from
       // failing the test
-      return false
+      return false;
     });
 
-    cy.wrap('passed').as('ctrl')
-    cy.get("a:not([href*='mailto:]']").each($el => {
-
+    cy.wrap('passed').as('ctrl');
+    cy.get('a:not([href*=\'mailto:]\']').each(($el) => {
       if ($el.prop('href').length > 0) {
-        const message = $el.text()
-        expect($el, message).to.have.attr("href").not.contain("undefined")
-        cy.log($el.attr('href'))
+        const message = $el.text();
+        expect($el, message).to.have.attr('href').not.contain('undefined');
+        cy.log($el.attr('href'));
       }
     });
   });
